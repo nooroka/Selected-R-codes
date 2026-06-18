6d4dc582c72569e85dc298db9356989345e50b8a
nooroka <51080708+nooroka@users.noreply.github.com>
2026-06-18 11:19:11 +0400

    Refactor data processing functions in R script

--- abundance_matrix_patients.R
+++ abundance_matrix_patients.R
@@ -22,7 +22,7 @@ grouped_data <- meta_data_new_kyh %>%
   summarise(Values = paste(sample_id, collapse = ";")) %>%
   ungroup()
 
-# Функция для подвыборки столбцов из df2 на основе значений из df1
+
 select_and_rename_columns <- function(id, columns, df) {
   columns_to_select <- str_split(columns, ";")[[1]]
   selected_data <- df %>% select(all_of(columns_to_select))
@@ -32,40 +32,37 @@ select_and_rename_columns <- function(id, columns, df) {
 }
 
 
-# Создание нового датафрейма для хранения результатов
-#new_df <- bind_rows(mapply(select_and_rename_columns, grouped_data$ID_KYH, grouped_data$Values, MoreArgs = list(df = data), SIMPLIFY = FALSE)) №не применять на большом объеме данных, зависает!
 
 result_list <- map2(grouped_data$ID_KYH, grouped_data$Values, ~select_and_rename_columns(.x, .y, df = data))
 library(purrr)
 library(readr)
 
-# Определяем вашу функцию update_column_names
+
 update_column_names <- function(df, name_file, sign) {
-  # Чтение файла с нужными частями имен
+
   required_names <- read_lines(name_file)
   
-  # Обработка имен колонок
+
   new_colnames <- sapply(colnames(df), function(colname) {
-    # Разделение имени по "_"
+
     parts <- str_split(colname, "_")[[1]]
     parts2 <- paste0(parts[2], "_", parts[3])
-    # Проверка, если вторая часть имени в списке нужных частей имен
+   
     if (length(parts) > 1 && parts2 %in% required_names) {
       return(paste0(colname, sign))
     } else {
       return(colname)
     }
   })
-  # Обновление имен колонок в датафрейме
+
   colnames(df) <- new_colnames
   return(df)
 }
 
-# Определяем вашу функцию calculate_fold_change
 calculate_fold_change <- function(df) {
-  colnames_list <- colnames(df)[-1]  # Исключаем первую колонку (Rownames)
+  colnames_list <- colnames(df)[-1] 
   
-  # Разделение имен колонок и создание таблицы с частями имен
+
   parts <- str_split(colnames_list, "_")
   first_parts <- sapply(strsplit(colnames_list, "_"), `[`, 1)
   second_parts <- paste0(sapply(strsplit(colnames_list, "_"), `[`, 2), "_", sapply(strsplit(colnames_list, "_"), `[`, 3))
@@ -73,17 +70,17 @@ calculate_fold_change <- function(df) {
   parts_df <- data.frame(do.call(rbind, parts), stringsAsFactors = FALSE)
   colnames(parts_df) <- c("Part1", "Part2", "Part3", "Year")
   
-  # Фильтрация колонок по последней части имени
+
   filtered_df <- parts_df %>% filter(Year %in% c("2017", "2022"))
   
-  # Поиск уникальных первых частей
+
   unique_parts <- unique(filtered_df$Part1)
   
-  # Создание нового датафрейма для хранения результатов
+  
   result <- df %>% select(Rownames)
   
   for (part in unique_parts) {
-    # Найти соответствующие колонки для текущей первой части
+  
     cols_2017 <- colnames_list[filtered_df$Part1 == part & parts_df$Year == "2017"]
     cols_2022 <- colnames_list[filtered_df$Part1 == part & parts_df$Year == "2022"]
     if (length(cols_2017) > 0 && length(cols_2022) > 0) {
@@ -94,7 +91,7 @@ calculate_fold_change <- function(df) {
         
         if (col_2022_first %in% first_parts) {
           index2 <- which(first_parts == col_2022_first)
-          #print(index2)
+         
           if (length(index2) > 0) {
             if (last_parts[index2[1]] == "2022") {
               index <- index2[1]
@@ -127,8 +124,7 @@ calculate_fold_change <- function(df) {
   
   return(result)
 }
-#учитывается только первый индекс из дубликатов
-# Функция для обработки каждого элемента списка result_list
+
 process_element <- function(i, result_list, data) {
   result_df <- data.frame(result_list[[i]])
   result_df$Rownames <- data$Rownames
@@ -142,17 +138,13 @@ process_element <- function(i, result_list, data) {
   
   df_combined <- update_column_names(df_combined, "../files/names_2017.txt", "_2017")
   df_combined <- update_column_names(df_combined, "../files/names_2022.txt", "_2022")
-  # print(colnames(df_combined))
+  
   df <- calculate_fold_change(df_combined)
   
   return(df)
 }
 result_dfs <- vector("list", length(result_list))
 
-# Using a for loop to achieve the same result as lapply
-#for (i in 1:length(result_list)) {
- #result_dfs[[i]] <- process_element(i, result_list = result_list, data= data)
-#}
 
 
 result_dfs <- lapply(1:length(result_list), process_element, result_list = result_list, data = data)
